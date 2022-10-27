155568683339b8889e25a04d6b6bec2835422739
Hanson-joy <106768228+Hanson-joy@users.noreply.github.com>
2022-10-27 23:51:02 +0300

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,10 +7,5 @@
 def lookup(obj):
     """This functions looks out for all attributes and methods of an object"""
     return dir(obj)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
 
 
